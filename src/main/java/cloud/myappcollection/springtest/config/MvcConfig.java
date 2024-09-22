2234ca87b6d368e56b66c05ab8662ccdee8866a5
shivang991 <32.shivangdubey@gmail.com>
2024-09-22 20:17:00 +0530

    feat: configure spring boot to serve nuxt build

---
+++ src/main/java/cloud/myappcollection/springtest/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package cloud.myappcollection.springtest.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ErrorPageRegistrar errorPageRegistrar() {
+        return registry -> {
+            registry.addErrorPages(
+                    new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+        };
+    }
+
+}
